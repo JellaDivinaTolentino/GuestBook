7561a5fea5d0fbffc97bb2453daaa29ab804de59
Jella Tolentino <jellatolentino@Jellas-MacBook-Air.local>
2019-03-29 15:58:39 +0800

    include favico

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,6 +1,6 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-
+<link rel="icon" href="favicon.ico" type="image/x-icon">
 <title>Guest Book</title>
 
 <!-- css -->
